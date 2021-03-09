4d8bfdf7a0c9eddaa7bbc532cfc9211aebe247e4
Lui Villegas <luipalencia@gmail.com>
2021-03-09 12:45:05 -0300

    Se agregaron const en app.js y botones en main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,31 @@
-import App from './components/App.js';
+// import App from './components/App.js';
+// import gamePage from './components/App.js';
+
+import {App, gamePageEasy, gamePageMedium, gamePageHard, congratsPage} from './components/App.js';
 
 document.getElementById('root').appendChild(App());
+
+
+let buttonToEasy = document.getElementById('easy');
+buttonToEasy.addEventListener("click", function () {
+    
+    document.getElementById("root").style.display = "none";
+    document.getElementById('gamePageEasy').appendChild(gamePageEasy());
+});
+
+
+let buttonToMedium = document.getElementById('medium');
+buttonToMedium.addEventListener("click", function () {
+    
+    document.getElementById("root").style.display = "none";
+    document.getElementById('gamePageMedium').appendChild(gamePageMedium());
+
+});
+
+let buttonToHard = document.getElementById('hard');
+buttonToHard.addEventListener("click", function () {
+    
+    document.getElementById("root").style.display = "none";
+    document.getElementById('gamePageHard').appendChild(gamePageHard());
+
+});
